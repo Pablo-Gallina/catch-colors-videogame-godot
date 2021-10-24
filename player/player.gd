4c1e54ed19935a1362555fc3fbb2be9a2c06cdef
Pablo Gallina <pablogallinag@gmail.com>
2021-10-23 20:05:02 -0600

    Limite de pantalla

--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
-export (int) var speed = 60
+export (int) var speed = 120
 const RIGHT : int = 1 #movimiento
 const LEFT : int = -1 #movimiento
 var iniciar : bool = false
 var movimiento : Vector2 = Vector2()
-
+var limite
 
 func _ready():
+	limite= get_viewport_rect().size
 	pass
 
 func _physics_process(delta):
 	movement(delta)
+	limitScreen()
 
 func movement(_delta):
 	movimiento = Vector2()	
@@ -26,3 +28,17 @@ func movement(_delta):
 	if movimiento.length() > 0:
 		movimiento=movimiento.normalized() * speed
 	position += movimiento*_delta
+
+func limitScreen():
+	position.x= clamp(position.x,18,limite.x-15)
+	position.y= clamp(position.y,20,limite.y-27)	
+
+"""func animation():
+	if movimiento.x != 0:
+		$AnimatedSprite.animation='move'
+		$AnimatedSprite.flip_h= movimiento.x<0
+	elif movimiento.y !=0:
+		$AnimatedSprite.animation='move'
+		$AnimatedSprite.flip_h= movimiento.y<0
+	else:
+		$AnimatedSprite.animation='idle'"""
