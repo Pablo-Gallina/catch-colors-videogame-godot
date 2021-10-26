b696d52bf9ff89ae0c0a06fd38c98f49a6fd60b3
Pablo Gallina <pablogallinag@gmail.com>
2021-10-26 17:44:24 -0600

    Agregando funcionalidad del movimiento del player sobre las plataformas y el game over

--- player/player.gd
+++ player/player.gd
@@ -1,27 +1,30 @@
 extends KinematicBody2D
 
-export (int) var distance_mov = 8 #Velocidad del player
+export (int) var distance_mov = 64 #Velocidad del player
 var iniciar : bool = false #Juego en curso
-var movimiento : Vector2 = Vector2() #movimiento del player
 var limite : Vector2 = Vector2() #Limite de la pantalla
+var movimiento : int = 0
+var col_path : int = 1
 
 func _ready():
 	limite= get_viewport_rect().size #Inicializar la variable, obteniendo el tamaño de la pantalla
-
 func _physics_process(delta):
 	movement(delta) #usado la funcion de movimiento del player
 
 func movement(_delta):
-	movimiento = Vector2()	#Resetear valores del movimiento
-	if Input.is_action_pressed("rigth"):
+	if Input.is_action_just_pressed("rigth"):
 		position.x += distance_mov
-	if Input.is_action_pressed("left"):
+		col_path +=1
+	if Input.is_action_just_pressed("left"):
 		position.x -= distance_mov
-	if Input.is_action_pressed("up"):
+		col_path +=1
+	if Input.is_action_just_pressed("up"):
 		position.y -= distance_mov
-	if Input.is_action_pressed("down"):
-		position.y += distance_mov
-
+		col_path +=1
+	if Input.is_action_just_pressed("down"):
+		position.y += distance_mov  
+		col_path +=1 
+	game_over()
 """func animation():
 	if movimiento.x != 0:
 		$AnimatedSprite.animation='move'
@@ -31,3 +34,11 @@ func movement(_delta):
 		$AnimatedSprite.flip_h= movimiento.y<0
 	else:
 		$AnimatedSprite.animation='idle'"""
+func game_over():
+	if movimiento<col_path:
+		print("game over")
+	else:
+		print("Continua", movimiento, col_path)
+
+func _on_path2_body_entered(body):
+	movimiento += 1
