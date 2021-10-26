e950e7e25289fe97edad95963df4ab1998bf7ab2
Pablo Gallina <pablogallinag@gmail.com>
2021-10-26 16:18:06 -0600

    Modificacion del movimiento del player

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var speed = 120 #Velocidad del player
+export (int) var distance_mov = 8 #Velocidad del player
 var iniciar : bool = false #Juego en curso
 var movimiento : Vector2 = Vector2() #movimiento del player
 var limite : Vector2 = Vector2() #Limite de la pantalla
@@ -10,27 +10,17 @@ func _ready():
 
 func _physics_process(delta):
 	movement(delta) #usado la funcion de movimiento del player
-	limitScreen() #usando la funcion de limite de pantalla para que el player no salga de esta pantalla
 
 func movement(_delta):
 	movimiento = Vector2()	#Resetear valores del movimiento
 	if Input.is_action_pressed("rigth"):
-		movimiento.x += 1
+		position.x += distance_mov
 	if Input.is_action_pressed("left"):
-		movimiento.x -= 1
+		position.x -= distance_mov
 	if Input.is_action_pressed("up"):
-		movimiento.y -= 1
+		position.y -= distance_mov
 	if Input.is_action_pressed("down"):
-		movimiento.y += 1
-	#Si en la variable movimiento : Vector2, su length sea > 0
-	if movimiento.length() > 0:
-		movimiento=movimiento.normalized() * speed #Normalizar el movimiento para que cuando se presionen 2 teclas, este no aumente * 2 su velocidad
-	position += movimiento*_delta #Mover al player
-
-func limitScreen():
-	#Limitar al personaje
-	position.x= clamp(position.x,18,limite.x-15)
-	position.y= clamp(position.y,20,limite.y-27)	
+		position.y += distance_mov
 
 """func animation():
 	if movimiento.x != 0:
